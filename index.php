c9cbccf7271bd173a4ea30f34e04dd9fe120d246
Palomita9191 <134742994+Palomita9191@users.noreply.github.com>
2023-06-09 15:54:41 -0400

    Lixia_web_development_env_1_project
    About me

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo"About me";
+    About me
+    Hello and welcome to my About page! My name is Li Xia, and I am delighted to share a little bit about myself with you.
+
+    I am a passionate individual who is constantly seeking knowledge and new experiences. Throughout my life, I have developed a diverse set of interests and skills that have shaped me into the person I am today. Let me take you on a journey through some of the key aspects of my life.
+
+    have been interested in art since I was a child, and graduated from the Art Design of Jiangnan University in China.After graduating from university, I worked as a graphic designer in Shanghai, China for three years.After I got married and had my daughter I became a full-time mother. Out of love for children, my friends and I founded a parent-child reading non-profit organization. So far, our member mothers have reached more than 3,000. We study with our children and grow up healthily and happily.At the same time, I founded an art training school - Golden Microphone Art Troupe. Then I accompanied my daughter to study in Canada and continued to be my full-time mother.
+
+    I have not worked during this time I often go to the community to do volunteer work,I teach adults and children to do crafts at community events for every Chinese Lunar New Year, I designed posters, designed and made stage backdrops for the NDG community. For the Multicultural Arts Festival, I designed and made decorations for traditional Chinese art for the NDG commity center I designed and made a lot of traditional Chinese crafts and teach adults and children how to make them.I used to go to women on the  rise to help women and children, I read stories to kids, play games with them, feed them and put them to bed.I design and make large puppets for the parade.I also often help the elderly Chinese in the community.etc.I am passionate about making a positive difference in the world around me. I actively seek opportunities to contribute to causes I care about, whether it is through volunteering, supporting charitable organizations, or advocating for social and environmental justice.Helping others makes me feel very happy.
+
+    As a multifaceted individual, I find joy in maintaining a balanced lifestyle. I appreciate spending time outdoors, surrounded by nature's wonders, as it helps me find solace and rejuvenation. Engaging in physical activities such as jogging, hiking, cycling, and practicing yoga allows me to maintain a healthy mind-body connection.
+
+    Thank you for takding the time to learn a little bit about me;
+    
+    ?>
+
+</body>
+</html>   
